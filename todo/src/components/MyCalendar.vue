6fefd59db262717d3fac8c9a0ec743206a7e970f
Никита Комаров <nikitakmrv76@gmail.com>
2024-01-01 15:47:28 +0300

    added fixed

--- todo/src/components/MyCalendar.vue
+++ todo/src/components/MyCalendar.vue
@@ -1,12 +1,41 @@
 <template>
     <div>
-        
+        <div>
+            <button>&lt</button>
+            <h1></h1>
+            <button>&gt</button>
+
+        </div>
+        <div>
+
+        </div>
     </div>
 </template>
 
 <script>
 export default{
+    data(){
+        return{
+            curentDate: new Date()
+        }
+    },
+    computed:{
+        currentMonth(){
 
+        },
+        daysInMonth(){
+
+        }
+    },
+    methods:{
+        previousMonth(){
+
+        },
+        nextMonth(){
+            
+        }
+    },
+    
 }
 </script>
 
